74296893cc8945ebc93ff90f51c101f46cc78c66
Andres Sena <andresrobertosena@gmail.com>
2025-04-22 21:03:31 -0300

    añadida la vista de terminos

---
+++ app/Views/terminos.php
@@ -0,0 +1,116 @@
+<main class="container-fluid mx-4">
+  <h1 class="text-center my-3 my-md-4">
+    Términos y condiciones de
+    <span class="fw-bold text-decoration-underline">Insumos_FAT</span>
+  </h1>
+
+  <div class="row" style="text-align: justify">
+    <div class="col-md-8 offset-md-2 mt-3">
+      <p class="lead">
+        En <span class="fw-bold">Insumos_FAT</span>, valoramos tu privacidad y nos
+        comprometemos a proteger la información personal que nos proporcionas
+        cuando utilizas nuestros servicios en línea. Esta Política de Privacidad
+        Extendida describe cómo recopilamos, utilizamos, compartimos y
+        protegemos tu información cuando interactúas con nuestra tienda en
+        línea, ya sea a través de nuestro sitio web o de nuestra aplicación
+        móvil.
+      </p>
+
+      <h2>1. Información que Recopilamos:</h2>
+      <ul>
+        <li>
+          <strong>Información de Identificación Personal:</strong> Recopilamos
+          información como tu nombre, dirección de correo electrónico, dirección
+          de envío y número de teléfono cuando creas una cuenta, realizas una
+          compra o nos contactas para solicitar asistencia.
+        </li>
+        <li>
+          <strong>Información de Pago:</strong> Cuando realizas una compra,
+          recopilamos información de pago, como números de tarjeta de crédito o
+          débito, para procesar tus transacciones de manera segura y eficiente.
+        </li>
+        <li>
+          <strong>Información de Uso:</strong> Recopilamos información sobre
+          cómo interactúas con nuestro sitio web o nuestra aplicación, incluidos
+          los libros que visualizas, añades al carrito o compras, así como tu
+          actividad de navegación y patrones de uso.
+        </li>
+        <li>
+          <strong>Información de Dispositivos:</strong> Podemos recopilar
+          información sobre el dispositivo que utilizas para acceder a nuestros
+          servicios, como tu dirección IP, tipo de navegador, sistema operativo
+          y identificadores únicos de dispositivos.
+        </li>
+      </ul>
+
+      <h2>2. Uso de la Información:</h2>
+      <p class="lead">
+        Utilizamos la información que recopilamos para procesar tus pedidos,
+        proporcionarte servicios personalizados, mejorar la funcionalidad de
+        nuestro sitio web y aplicación, y comunicarnos contigo sobre tu cuenta y
+        tus compras. También podemos utilizar tu información para enviarte
+        correos electrónicos promocionales, boletines informativos u otra
+        información relevante sobre nuestros productos y servicios, siempre y
+        cuando hayas dado tu consentimiento para recibir dichas comunicaciones.
+      </p>
+
+      <h2>3. Compartir Información:</h2>
+      <p class="lead">
+        No vendemos, alquilamos ni compartimos tu información personal con
+        terceros con fines comerciales sin tu consentimiento expreso, excepto
+        según lo requiera la ley o en cumplimiento de una orden judicial.
+        Podemos compartir tu información con proveedores de servicios de
+        confianza que nos ayudan a operar nuestro negocio y prestar servicios a
+        nuestros clientes, como procesadores de pagos, servicios de envío y
+        proveedores de alojamiento web.
+      </p>
+
+      <h2>4. Seguridad de la Información:</h2>
+      <p class="lead">
+        Implementamos medidas de seguridad técnicas, administrativas y físicas
+        para proteger tu información contra accesos no autorizados, divulgación,
+        alteración o destrucción. Sin embargo, debes tener en cuenta que ninguna
+        medida de seguridad es absolutamente infalible, y no podemos garantizar
+        la seguridad absoluta de tu información en todo momento.
+      </p>
+
+      <h2>5. Tus Derechos:</h2>
+      <p class="lead">
+        Tienes derecho a acceder, corregir, actualizar o eliminar tu información
+        personal en cualquier momento. Puedes hacerlo iniciando sesión en tu
+        cuenta o contactándonos directamente. También tienes derecho a optar por
+        no recibir comunicaciones de marketing por correo electrónico en
+        cualquier momento, siguiendo las instrucciones de cancelación de la
+        suscripción incluidas en cada correo electrónico o contactándonos para
+        actualizar tus preferencias de comunicación.
+      </p>
+
+      <h2>6. Menores de Edad:</h2>
+      <p class="lead">
+        Nuestros servicios no están dirigidos a menores de 18 años, y no
+        recopilamos de manera consciente información personal de personas
+        menores de esa edad. Si tienes menos de 18 años, no utilices nuestros
+        servicios ni nos proporciones información personal.
+      </p>
+
+      <h2>7. Cambios en esta Política:</h2>
+      <p class="lead">
+        Nos reservamos el derecho de actualizar o modificar esta Política de
+        Privacidad en cualquier momento. Cualquier cambio entrará en vigor
+        inmediatamente después de su publicación en nuestro sitio web. Te
+        recomendamos que revises periódicamente esta política para estar al
+        tanto de cualquier cambio.
+      </p>
+
+      <p class="lead">
+        Al utilizar nuestros servicios, aceptas los términos y condiciones
+        establecidos en esta Política de Privacidad. Si tienes alguna pregunta o
+        inquietud sobre nuestra política o prácticas de privacidad, no dudes en
+        contactarnos.
+      </p>
+
+      <p class="lead">Fecha de Entrada en Vigor: <span><script>document.write(new Date().getDay());</script></span>/<span><script>document.write(new Date().getMonth());</script></span>/<span><script>document.write(new Date().getFullYear());</script></span></p>
+      <p class="lead">Última Actualización: <span><script>document.write(new Date().getDay());</script></span>/<span><script>document.write(new Date().getMonth());</script></span>/<span><script>document.write(new Date().getFullYear());</script></span></p>
+    </div>
+  </div>
+</main>
