a599fdfa640c36de48952344b69c1a83178b8a14
Andres Sena <andresrobertosena@gmail.com>
2025-04-30 21:08:54 -0300

    Minor fixes en el controlador de modal_development

--- app/Views/contacto.php
+++ app/Views/contacto.php
@@ -1,3 +1,20 @@
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Funcionalidad en Desarrollo</h1>
+        <button type="button" class="ms-auto border-0 focus-none" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-circle-xmark" style="color: red !important;"></i></button>
+      </div>
+      <div class="modal-body">
+        Estamos desarrollando esta funcionalidad. ¡Gracias por tu paciencia!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <main class="container-fluid my-4">
   <div class="m-md-3">
       <section class="showcase py-5 rounded-4 shadow-lg" style="border: 1px solid black;">
