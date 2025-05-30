1d18a827635c2f7a6fa2b16073c00fcf656114e0
Notasatomicas <notasatomicas@gmail.com>
2025-05-30 14:00:18 -0300

    Eliminado js para mostrar elemento activo en navbar y mejorada lógica en php del nav

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,85 +1,83 @@
+<?php
+  $uri = service('uri');
+  $segment = $uri->getSegment(1);
+
+  function esActivo($ruta, $segment) {
+    return $ruta === $segment || ($ruta === 'inicio' && $segment === '');
+  }
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large">
   <div class="container-fluid">
-
-    <!-- El titulo del navbar, nada mas -->
     <a class="navbar-brand animate__bounceIn" href="<?= base_url('/') ?>">
       <img src="<?= base_url('public/assets/img/icon.png') ?>" alt="Logo" width="57" height="50" class="d-inline-block">
       Insumos_FAT
     </a>
 
-    <!-- Este es el toggler que solo se ve cuando se hace offcanvas -->
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
       <i class="fa-solid fa-bars"></i>
     </button>
-    <!-- Aca finaliza (esto es solo el boton viejo, nada mas) lo separo por las dudas -->
 
-    <!-- esta parte queda offcanvas -->
-    <!-- tengase en cuenta que si a esto le agrego la clase show se va a mostrar el offcanvas permanentemente (para fines de desarrollo obvio) -->
     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
-
-      <!-- lo que se muestra en la cabecera en los dispositivos chiquitos -->
       <div class="offcanvas-header">
-
-        <!-- Por el momento voy a comentar el titulo del offcanvas porque no lo veo relevante -->
-        <!-- <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Offcanvas</h5> -->
-
         <button type="button" class="btn" data-bs-dismiss="offcanvas" aria-label="Close" style="color: red;">
           <i class="fa-solid fa-circle-xmark" style="color: red !important;"></i>
         </button>
       </div>
 
-      <!-- lo que se muestra en el body en los dispositivos chiquitos -->
       <div class="offcanvas-body">
-
-        <!-- clases originales que tenia -> navbar-nav justify-content-end flex-grow-1 pe-3 -->
         <ul class="navbar-nav nav-pills justify-content-end flex-grow-1 pe-3">
 
-          <li class="hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0">
-            <a id="btn_nav_inicio" class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('inicio') ?>">
-              <i class="ms-md-0 me-1 fa-solid fa-house"></i>
+          <!-- INICIO -->
+          <li class="<?= esActivo('inicio', $segment) ? 'nav-item mx-5 mx-sm-4 mx-lg-0' : 'hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0' ?>">
+            <a class="nav-link text-center me-lg-2 px-2 px-xl-4 <?= esActivo('inicio', $segment) ? 'active' : '' ?>" href="<?= base_url('inicio') ?>">
+              <i class="ms-md-0 me-1 fa-solid fa-house <?= esActivo('inicio', $segment) ? 'animate__animated animate__heartBeat animate__infinite' : '' ?>"></i>
               Inicio
             </a>
           </li>
 
-          <li class="hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0">
-            <a id="btn_nav_comercializacion" class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('comercializacion') ?>">
-              <i class="ms-md-0 me-1 fa-solid fa-truck-arrow-right"></i>
+          <!-- COMERCIALIZACION -->
+          <li class="<?= esActivo('comercializacion', $segment) ? 'nav-item mx-5 mx-sm-4 mx-lg-0' : 'hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0' ?>">
+            <a class="nav-link text-center me-lg-2 px-2 px-xl-4 <?= esActivo('comercializacion', $segment) ? 'active' : '' ?>" href="<?= base_url('comercializacion') ?>">
+              <i class="ms-md-0 me-1 fa-solid fa-truck-arrow-right <?= esActivo('comercializacion', $segment) ? 'animate__animated animate__heartBeat animate__infinite' : '' ?>"></i>
               Comercializacion
             </a>
           </li>
 
-          <li class="hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0">
-            <a id="btn_nav_nosotros" class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('nosotros') ?>">
-              <i class="ms-md-0 me-1 fa-solid fa-face-grin-wink"></i>
+          <!-- NOSOTROS -->
+          <li class="<?= esActivo('nosotros', $segment) ? 'nav-item mx-5 mx-sm-4 mx-lg-0' : 'hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0' ?>">
+            <a class="nav-link text-center me-lg-2 px-2 px-xl-4 <?= esActivo('nosotros', $segment) ? 'active' : '' ?>" href="<?= base_url('nosotros') ?>">
+              <i class="ms-md-0 me-1 fa-solid fa-face-grin-wink <?= esActivo('nosotros', $segment) ? 'animate__animated animate__heartBeat animate__infinite' : '' ?>"></i>
               Quienes somos
             </a>
           </li>
 
-          <li class="hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0">
-            <a id="btn_nav_contacto" class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('contacto') ?>">
-              <i class="ms-md-0 me-1 fa-solid fa-phone-volume"></i>
+          <!-- CONTACTO -->
+          <li class="<?= esActivo('contacto', $segment) ? 'nav-item mx-5 mx-sm-4 mx-lg-0' : 'hvr-underline-from-center nav-item mx-5 mx-sm-4 mx-lg-0' ?>">
+            <a class="nav-link text-center me-lg-2 px-2 px-xl-4 <?= esActivo('contacto', $segment) ? 'active' : '' ?>" href="<?= base_url('contacto') ?>">
+              <i class="ms-md-0 me-1 fa-solid fa-phone-volume <?= esActivo('contacto', $segment) ? 'animate__animated animate__heartBeat animate__infinite' : '' ?>"></i>
               Contacto
             </a>
           </li>
 
+          <!-- LOGIN / LOGOUT -->
           <li class="nav-item mx-5 mx-sm-4 mx-lg-0">
             <?php if (session()->get('id_usuario') || session()->get('isLoggedIn')): ?>
-              <!-- Usuario logueado - Mostrar botón de cerrar sesión -->
-              <a id="btn_nav_logout" class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('auth/logout') ?>" style="background-color: #dc3545 !important; color: white !important;">
+              <!-- LOGOUT -->
+              <a class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('auth/logout') ?>" style="background-color: #dc3545 !important; color: white !important;">
                 <i class="ms-md-0 me-1 fa-solid fa-sign-out-alt"></i>
                 Cerrar sesión
               </a>
             <?php else: ?>
-              <!-- Usuario no logueado - Mostrar botón de iniciar sesión -->
-              <a id="btn_nav_login" class="nav-link text-center me-lg-2 px-2 px-xl-4" href="<?= base_url('auth') ?>" style="background-color:rgb(24, 167, 155) !important; color: white !important;">
-                <i class="ms-md-0 me-1 fa-solid fa-sign-in-alt"></i>
+              <!-- LOGIN -->
+              <a class="nav-link text-center me-lg-2 px-2 px-xl-4 <?= esActivo('auth', $segment) ? 'active' : '' ?>" href="<?= base_url('auth') ?>" style="background-color:rgb(24, 167, 155) !important; color: white !important;">
+                <i class="ms-md-0 me-1 fa-solid fa-sign-in-alt <?= esActivo('auth', $segment) ? 'animate__animated animate__heartBeat animate__infinite' : '' ?>"></i>
                 Iniciar sesión
               </a>
             <?php endif; ?>
           </li>
 
         </ul>
-
       </div>
     </div>
   </div>
