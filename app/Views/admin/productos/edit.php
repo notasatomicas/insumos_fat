d23cbabf36b69eb3f4c93f9d9f66d1916eabfafe
Andres Sena <andresrobertosena@gmail.com>
2025-06-14 23:44:17 -0300

    Modularizado el sidebar

--- app/Views/admin/productos/edit.php
+++ app/Views/admin/productos/edit.php
@@ -12,25 +12,7 @@
     <main class="container my-4 flex-fill">
         <div class="row">
             <!-- Sidebar de administración -->
-            <div class="col-md-3">
-                <div class="list-group">
-                    <a href="<?= site_url('admin/dashboard') ?>" class="list-group-item list-group-item-action">
-                        <i class="fa-solid fa-gauge me-2"></i> Dashboard
-                    </a>
-                    <a href="<?= site_url('admin/users') ?>" class="list-group-item list-group-item-action">
-                        <i class="fa-solid fa-user me-2"></i> Usuarios
-                    </a>
-                    <a href="<?= site_url('admin/productos') ?>" class="list-group-item list-group-item-action active">
-                        <i class="fa-solid fa-box me-2"></i> Productos
-                    </a>
-                    <a href="<?= site_url('admin/categorias') ?>" class="list-group-item list-group-item-action">
-                        <i class="fa-solid fa-table-list me-2"></i> Categorías
-                    </a>
-                    <a href="<?= site_url('admin/contactos') ?>" class="list-group-item list-group-item-action">
-                        <i class="fa-solid fa-envelope me-2"></i> Mensajes de Contacto
-                    </a>
-                </div>
-            </div>
+            <?= view('admin/sidebar') ?>
             
             <!-- Contenido principal -->
             <div class="col-md-9">
